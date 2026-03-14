8ff7bd0d844db4777626825f1724a1e68dfa7bfc
silomentus <silomentus@gmail.com>
2026-03-14 15:20:10 +0600

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -36,7 +37,10 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="antialiased noise grid-bg">{children}</body>
+      <body className="antialiased noise grid-bg">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
